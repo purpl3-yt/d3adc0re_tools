4f373557d5722af4eacbbff685205bea80cb84b7
purpl3-yt <remezdevid2000@gmail.com>
2023-04-22 12:42:44 +0300

    123

--- d3adc0re_tools/__init__.py
+++ d3adc0re_tools/__init__.py
@@ -9,9 +9,6 @@ print('''
  |____/____/ \__,_|\__,_|\___|\___/|_|  \___|   |_|\___/ \___/|_|___/
                                                                      by purpl3''')
 
-# Clean pycache folders
-import pathlib, shutil; [shutil.rmtree(p.__str__()) for p in pathlib.Path('.').rglob('__pycache__')]
-
 # Import programs
-from . import PyProxy
-from . import SongDownloader
+from .PyProxy import *
+from .SongDownloader import *
